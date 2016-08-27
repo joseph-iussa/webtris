8260036120e92219d776cea19560c9f248ef87ae
Joseph Iussa <jiussa@internode.on.net>
2016-08-28 01:02:46 +0930

    Build dir is now removed before build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require("path");
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const IN_PROD = process.env.NODE_ENV === "production";
 
@@ -23,7 +24,8 @@ function getPlugins() {
                 compress: {
                     warnings: false
                 }
-            })
+            }),
+            new CleanWebpackPlugin(["dist"])
         ]);
     }
 
